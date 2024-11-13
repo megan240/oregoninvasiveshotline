bde34915fccbfae42cbe28a54fe515b29334a86e
Taylor Hughes <tahughes@pdx.edu>
2024-11-12 16:15:57 -0800

    Added event handler for small navigation hamburger menu toggling

--- oregoninvasiveshotline/static/js/main.js
+++ oregoninvasiveshotline/static/js/main.js
@@ -81,3 +81,39 @@ function getDefaultMapOptions(){
 
     return mapOptions;
 }
+
+// Code to allow the Hamburger Menu to be functional on small pages
+const smallNavToggle = document.querySelector('#navbar-toggler');
+// const theSmallHeaderElement = document.querySelector('#small-screen-header nav');
+const theSmallNavElement = document.querySelector('#small-nav-bar');
+const theMainElement = document.querySelector('#main');
+
+
+const toggleCollapser = () => {
+    const lengthOfArr = theSmallNavElement.getAttribute("class");
+    // if uncollapsed, let's collapse it by removing class="uncollapsed-block"
+    // and adding class="collapsed-block"
+    if(lengthOfArr.includes("uncollapsed-block")){ // note "un..." evaluated first
+        theMainElement.classList.remove("collapsed-block");
+        theMainElement.classList.add("uncollapsed-block");
+        theSmallNavElement.classList.remove("uncollapsed-block");
+        theSmallNavElement.classList.add("collapsed-block");
+    }
+    else if(lengthOfArr.includes("collapsed-block")){
+        theMainElement.classList.remove("uncollapsed-block");
+        theMainElement.classList.add("collapsed-block");
+        theSmallNavElement.classList.remove("collapsed-block");
+        theSmallNavElement.classList.add("uncollapsed-block");
+    }
+    else{
+        console.log("error");
+    }
+};
+
+smallNavToggle.addEventListener('click', toggleCollapser);
+
+const getItemList = document.querySelectorAll('.sm-nav-list-item a');
+
+for(let i = 0; i < getItemList.length; i++){
+    getItemList[i].addEventListener('click', toggleCollapser);
+}
